2dc95a8e4266f3f8d9a6b7111431325875320575
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-11-03 14:39:20 -0800

    improvement: separate nodes and edges by phase

--- server/main.cpp
+++ server/main.cpp
@@ -15,25 +15,40 @@ std::shared_ptr<Edge> makeEdge(std::string id, int distance)
 
 int main()
 {
-    std::vector<std::shared_ptr<Node>> nodes = {};
-    nodes.push_back(makeNode("A"));
-    nodes.push_back(makeNode("B"));
-    nodes.push_back(makeNode("C"));
-    nodes.push_back(makeNode("D"));
-    nodes.push_back(makeNode("E"));
-
+    std::vector<std::shared_ptr<Node>> nodes;
     std::vector<std::shared_ptr<Edge>> edges;
-    edges.push_back(makeEdge("a", 8));
-    edges.push_back(makeEdge("b", 9));
-    edges.push_back(makeEdge("c", 4));
-    edges.push_back(makeEdge("d", 5));
-    edges.push_back(makeEdge("e", 1));
-    edges.push_back(makeEdge("f", 6));
-    edges.push_back(makeEdge("g", 4));
-    edges.push_back(makeEdge("h", 2));
-    edges.push_back(makeEdge("i", 6));
-    edges.push_back(makeEdge("j", 9));
-    edges.push_back(makeEdge("k", 3));
+
+    std::shared_ptr<Node> startNode = makeNode("A");
+
+    std::vector<std::shared_ptr<Edge>> startToFirstEdges;
+    startToFirstEdges.push_back(makeEdge("a", 8));
+    startToFirstEdges.push_back(makeEdge("b", 9));
+    startToFirstEdges.push_back(makeEdge("c", 4));
+
+    std::vector<std::shared_ptr<Node>> firstNodes;
+    firstNodes.push_back(makeNode("B"));
+    firstNodes.push_back(makeNode("C"));
+    firstNodes.push_back(makeNode("D"));
+
+    std::vector<std::shared_ptr<Edge>> firstEdges;
+    firstEdges.push_back(makeEdge("d", 5));
+    firstEdges.push_back(makeEdge("e", 1));
+
+    std::vector<std::shared_ptr<Edge>> firstToSecondEdges;
+    firstToSecondEdges.push_back(makeEdge("f", 6));
+    firstToSecondEdges.push_back(makeEdge("g", 4));
+    firstToSecondEdges.push_back(makeEdge("h", 2));
+    firstToSecondEdges.push_back(makeEdge("i", 6));
+
+    std::vector<std::shared_ptr<Node>> secondNodes;
+    secondNodes.push_back(makeNode("E"));
+    secondNodes.push_back(makeNode("F"));
+
+    std::vector<std::shared_ptr<Edge>> secondToEndEdges;
+    secondToEndEdges.push_back(makeEdge("j", 9));
+    secondToEndEdges.push_back(makeEdge("k", 3));
+
+    std::shared_ptr<Node> endNode = makeNode("G");
 
     for (std::shared_ptr<Node> node : nodes)
     {
