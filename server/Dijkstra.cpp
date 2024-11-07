ae0ec37db30319dfbbd9bac1d97bcf3b7d8dfabe
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-11-06 20:39:46 -0800

    feat: set start node

--- server/Dijkstra.cpp
+++ server/Dijkstra.cpp
@@ -7,5 +7,8 @@ Dijkstra::Dijkstra(const std::shared_ptr<Graph> graph): graph(graph)
 
 int Dijkstra::calculateShortestPath(const std::string startNodeId, const std::string endNodeId)
 {
+  // TODO: Clear dynamic values
+
+  this->graph->nodes[startNodeId]->currentDistance = 0;
   return 100;
 }
